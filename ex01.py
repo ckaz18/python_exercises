daec31a4e3249aac1cbc241320b75fab39cc7357
Christi Kazakov <ckazakov18@gmail.com>
2018-05-25 18:44:20 -0600

    Update with basic math func

--- ex01.py
+++ ex01.py
@@ -1,1 +1,16 @@
+# Hello World Comment
 print("Hello World")
+a = 18
+b = 3
+c = a * b
+d = a ** b
+e = a / b
+f = a % b
+g = b % a
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(a >= b)
+print(b >= a)
